5ad025c3afbfe1143b408f28d3c77f0589ba161a
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-11-09 10:19:44 +0530

    calculated the employee wage(implemented UseCase 2)

--- EmployeeWageDemo/EmployeeWage.cs
+++ EmployeeWageDemo/EmployeeWage.cs
@@ -6,17 +6,27 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageDemo
 {
-    internal class Program
+    internal class EmployeeWage
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Demo program");
+            int dailyWage = 0;
+            int fullTime = 8;
             Random random = new Random();
             int EmployeeInput=random.Next(0,2);
             if (EmployeeInput == 1)
+            {
                 Console.WriteLine("Employee is present");
+                dailyWage = 20;
+            }
             else
+            {
                 Console.WriteLine("Employee is absent");
+                dailyWage = 0;
+            }    
+            int empWage = dailyWage * fullTime;
+            Console.WriteLine("Employee Wage:" + empWage);
             Console.ReadLine();
 
         }
